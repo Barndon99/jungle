d127005f311c879122cc9ed2dcb3179e9d8f5401
Brandon Rowlandson <rowlandsonbrandon@gmail.com>
2021-07-21 18:43:41 -0600

    Add admin dashboard to display categories and products

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -2,5 +2,7 @@ class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: "dhh", password: "secret", except:
 
   def show
+    @Category_count = Category.select(:categories).count
+    @Product_count = Product.select(:products).count
   end
 end
