afac023838564331a35aca77067996dbcc093db5
Brandon Rowlandson <rowlandsonbrandon@gmail.com>
2021-07-21 18:08:25 -0600

    Add basic authentication

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,6 @@
 class Admin::DashboardController < ApplicationController
+  http_basic_authenticate_with name: "dhh", password: "secret", except:
+
   def show
   end
 end
--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,4 +1,5 @@
 class Admin::ProductsController < ApplicationController
+  http_basic_authenticate_with name: "dhh", password: "secret", except: :index
 
   def index
     @products = Product.order(id: :desc).all
