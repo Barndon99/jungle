a237397be1da7351c7687f664521ab8a7148b60f
Brandon Rowlandson <rowlandsonbrandon@gmail.com>
2021-07-26 19:53:35 -0600

    Add validation for users

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,9 +4,32 @@ RSpec.describe User, type: :model do
 
   describe 'Validations' do
     it 'creates a user with valid fields' do
-    @no_blanks = User.new(fname: 'barndon', lname: 'rowland', email: 'row@gmail.com', password: 'hellothere', password_confirmation: 'hellothere')
-    expect(@no_blanks).to be_valid
+      @no_blanks = User.new(fname: 'barndon', lname: 'rowland', email: 'row@gmail.com', password: 'hellothere', password_confirmation: 'hellothere')
+      expect(@no_blanks).to be_valid
     end
+
+    it 'raises an error when first name is blank' do
+      @user = User.new(lname: 'rowland', email: 'row@gmail.com', password: 'hellothere', password_confirmation: 'hellothere')
+      expect(@user).not_to be_valid
+    end
+
+    it 'raises an error when last name is blank' do
+      @user = User.new(fname: 'rowland', email: 'row@gmail.com', password: 'hellothere', password_confirmation: 'hellothere')
+      expect(@user).not_to be_valid
+    end
+
+    it 'raises an error when no email is provided' do
+      @user = User.new(lname: 'rowland', email: 'row@gmail.com', password: 'hellothere', password_confirmation: 'hellothere')
+      expect(@user).not_to be_valid
+    end
+
+    it 'requires password and confirmation to match' do
+      @user1 = User.new(fname: 'barndon', lname: 'rowland', email: 'row@gmail.com', password: 'hellothere', password_confirmation: 'hellothere')
+      @user2 = User.new(fname: 'barndon', lname: 'rowland', email: 'row@gmail.com', password: 'hellothere', password_confirmation: 'thisdoesntmatch')
+      expect(@user1).to be_valid
+      expect(@user2).not_to be_valid
+    end
+
   end
 
   #describe '.authenticate_with_credentials' do
