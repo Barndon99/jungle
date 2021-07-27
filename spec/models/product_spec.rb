693a094b9ea9576889f1135ab2c3f30ba83d9fb8
Brandon Rowlandson <rowlandsonbrandon@gmail.com>
2021-07-26 18:54:53 -0600

    Add testing to Products model

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,7 +1,46 @@
 require 'rails_helper'
 
 RSpec.describe Product, type: :model do
-  describe 'Validations' do
-    # validation tests/examples here
+  before(:each) do
+    @category = Category.new(name: 'temp')
   end
-end 
+  context 'given a product' do
+    it 'accepts the product' do
+      @no_blanks = Product.new(name: 'hipster product1', price_cents: 3, quantity: 1, :category => @category)
+      expect(@no_blanks.errors).not_to include('can\'t be blank')
+    end
+  end
+
+  context 'given a product without a price' do
+    it 'should raise an error' do
+      @new_product = Product.new(name: 'someName', quantity: 1, :category => @category)
+      expect(@new_product).to_not be_valid
+      expect(@new_product.errors.messages[:price]).to include('can\'t be blank')
+    end
+  end
+
+  context 'given a product without a name' do
+    it 'should raise an error' do
+      @new_product = Product.new(price_cents: 5, quantity: 1, :category => @category)
+      expect(@new_product).to_not be_valid
+      expect(@new_product.errors.messages[:name]).to include('can\'t be blank')
+    end
+  end
+
+  context 'given a product without a quantity' do
+    it 'should raise an error' do
+      @new_product = Product.new(price_cents: 5, name: 'someName', :category => @category)
+      expect(@new_product).to_not be_valid
+      expect(@new_product.errors.messages[:quantity]).to include('can\'t be blank')
+    end
+  end
+
+  context 'given a product without a category' do
+    it 'should raise an error' do
+      @new_product = Product.new(price_cents: 5, quantity: 1, name: 'someName')
+      expect(@new_product).to_not be_valid
+      expect(@new_product.errors.messages[:category]).to include('can\'t be blank')
+    end
+  end
+
+end
